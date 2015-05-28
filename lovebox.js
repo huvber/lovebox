3d8a99d0b1753feb3cbd69d2127f21be0feb8882
Andrea Mannarà <a.mannara@qualitech.it>
2015-05-28 17:40:30 +0200

    Modificato tutto per smettere di usare le googleapi e usare un server su meteor. Download mp3 ma non suona e non si aggiorna

---
+++ lovebox.js
@@ -0,0 +1,160 @@
+var debug   = require('debug');
+var fs      = require('fs');
+var os      = require('os');
+var async   = require('async');
+var exec    = require('child_process').exec;
+var ddpi    = require('./server');
+var ison     = require('is-online');
+var dbg = {
+  //MAIN OUTPUT
+  m : debug('lovebox:main'),
+
+  //THROWING FUNCTION
+  t : debug('lovebox:throw'),
+  th : function(err){
+    this.t(err);
+    throw err;
+  },
+  //INFO
+  i: debug('lovebox:INFO'),
+  //ERROR
+  e: debug('lovebox:ERROR')
+};
+
+var Lovebox = function(){
+  this.config = undefined;
+  this.lovebox = undefined;
+  this.ddp = undefined;
+  this.isPlaying = false;
+  this.currentTrack = '';
+};
+
+// Method to read the config file.
+// cfg is the path of the file and as to be json
+// throw an error if the file doesn't exist or the JSON file
+// has syntax errors
+Lovebox.prototype.readConfig = function(cfgUrl){
+  var self = this;
+  if (cfgUrl === undefined || cfgUrl === '') {
+    cfgUrl = 'config.json';
+  }
+  this.config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+  return true && this.config;
+};
+Lovebox.prototype.connect2Wifi = function(){
+  var wifi = this.config.wifi;
+  for(var i in wifi){
+    for(var nif in os.networkInterfaces()){
+
+    }
+  }
+};
+var checkIsOnline = function(ison,isoff){
+  ison(function(err,online){
+    if(online){
+      ison();
+    }else{
+      isoff();
+      setTimeout(function(){
+        checkIsOnline(ison,isoff);
+      },3000);
+    }
+  });
+};
+var downloadMp3 = function(config,mp3){
+  var exec = require('child_process').exec;
+  var url = '\'http://' + config.server.host + ':' + config.server.port + mp3.url+'\'';
+  if (!fs.existsSync('mp3s/' + mp3.filename)) {
+    child = exec('wget -P mp3s/ ' + url,
+      function(error, stdout, stderr) {
+          if (error !== null)
+            dbg.e(error);
+        });
+  }
+};
+Lovebox.prototype.resetMp3 = function(handler){
+  var files = fs.readdirSync('./mp3s');
+  for (var f in files){
+    fs.unlinkSync('./mp3s/'+ files[f]);
+  }
+  handler();
+};
+Lovebox.prototype.playMusic = function(){
+  return true;
+};
+
+Lovebox.prototype.start = function(){
+  //check if is connected
+  var self = this;
+  checkIsOnline(function(){
+    //if is online
+    dbg.m('connected');
+    self.resetMp3(function(){
+      self.ddp.connect();
+    });
+  },function(){
+    dbg.m('not connected');
+    //if is off
+  });
+  self.playMusic();
+};
+
+// Function that start the lovebox daemon
+Lovebox.prototype.init = function(cfgUrl){
+  if (cfgUrl === undefined || cfgUrl === '') {
+    cfgUrl = 'config.json';
+  }
+  if(this.config === undefined )
+    this.readConfig(cfgUrl);
+  //read the config if the readConfig function wasn't used before
+  var self = this;
+  this.ddp = new ddpi({
+    config: this.config,
+    onGetLovebox: function(lovebox,mp3s){
+      dbg.m('get lovebox');
+      self.lovebox = lovebox;
+      for(var i in mp3s){
+        downloadMp3(self.config,mp3s[i]);
+      }
+      //store wifi setup on config
+      self.config.wifi = lovebox.wifi;
+      fs.writeFileSync('config.json', JSON.stringify(self.config, null, 4));
+    },
+    onLoveboxChange: function(lovebox,oldfields){
+      dbg.m('change lovebox');
+      self.lovebox = lovebox;
+      //store wifi setup on config
+      self.config.wifi = lovebox.wifi;
+      fs.writeFileSync('config.json', JSON.stringify(self.config, null, 4));
+
+      //check state changed
+      //CHANGE STATE
+
+    },
+    onMp3Added: function(mp3){
+      //save MP3
+      dbg.m(mp3);
+      downloadMp3(mp3);
+
+    },
+    onMp3Removed: function(oldValue){
+      //check if the mp3 is running
+
+      //remove MP3
+
+    },
+    onDisconnection: function(code,message){
+      dbg.e('Disconnection code:' +code);
+      dbg.e(message);
+      //try to reconnect with wifi
+
+
+    },
+    onConnectionError: function(error){
+      dbg.e(error);
+      //try to reconnect with wifi
+    }
+  });
+};
+
+module.exports = Lovebox;
---
+++ server.js
@@ -0,0 +1,129 @@
+var deb = require('debug')('debug:server');
+var log = require('debug')('debug:server-msg');
+var DDPClient = require('ddp');
+var DDPLogin = require('ddp-login');
+var _ = require('underscore');
+var async = require('async');
+
+function Server(data) {
+  this.config = data.config;
+  this.token = null;
+  this.lovebox = null;
+  this.ddp = new DDPClient({
+    host: this.config.server.host,
+    port: this.config.server.port,
+    autoReconnect: true,
+    autoReconnectTimer: 500,
+    maintainCollections: true,
+    useSockJs: true
+  });
+  log('DDP Client initialized');
+  this.data = data;
+
+  this.ddp.on('message', function(msg) {
+    //deb('ddp message: ' + msg);
+  });
+  this.ddp.on('socket-close', function(code, message) {
+    //deb('Close: %s %s', code, message);
+  });
+  this.ddp.on('socket-error', function(error) {
+    //deb('Error: %j', error);
+  });
+}
+
+
+Server.prototype.connect = function(func) {
+  log('DDPClient connection start');
+  var ddp = this.ddp;
+  var config = this.config;
+  var data = this.data;
+  var srv = this;
+  var token = this.token;
+
+  async.series([
+    function(c){
+      //connect;
+      ddp.connect(function(err, wrec) {
+        log('connection');
+        if (err)
+          log('connection error' + err);
+        else
+          log('connected');
+        c(err,null);
+      });
+    },
+    function(c){
+      //login
+      DDPLogin.loginWithEmail(ddp, config.server.user, config.server.passwd, function (err, userInfo) {
+        if (err)
+          log('login Error');
+        else{
+          log('logged');
+          token = userInfo.token;
+        }
+        c(err,null);
+      });
+    },
+    function(c){
+      //subscription and observation
+      ddp.subscribe('loveboxes');
+      ddp.subscribe('mp3s');
+
+      var obsLoveboxes =  ddp.observe('loveboxes');
+      obsLoveboxes.changed = function(idL, oldFields, clearedFields, newFields) {
+        log(srv.lovebox);
+        if (idL === config.loveboxId) {
+          _.extend(srv.lovebox, newFields);
+
+          if (data.onLoveboxChange !== undefined) data.onLoveboxChange(srv.lovebox, oldFields);
+        }
+      };
+
+      var obsMp3s = ddp.observe('mp3s');
+      obsMp3s.added = function(id) {
+        log('evvai');
+        mp3 = ddp.collections.mp3s.find(id);
+        if (config.loveboxId === mp3.lovebox) {
+          if (data.onMp3Added !== undefined) data.onMp3Added(mp3);
+        }
+      };
+      obsMp3s.removed = function(id, oldValue) {
+        if (data.onMp3Removed !== undefined) data.onMp3Removed(oldValue);
+      };
+      c();
+    },
+    function(c){
+      //retrieve lovebox
+      log('getting loveBox...');
+      ddp.call('getLovebox', [config.loveboxId], function(err, res) {
+        if (err)
+          log('getLovebox' + err);
+        else {
+          srv.lovebox = res;
+          ddp.call('mp3List',[config.loveboxId],function(err,res){
+            if(err)
+              log('getList error' + err);
+            else if (data.onGetLovebox !== undefined) data.onGetLovebox(srv.lovebox,res);
+            log(res);
+            c(err,null);
+          });
+        }
+      });
+    },
+
+  ],function c(err,data){
+    if(err) log(err);
+  });
+
+  ddp.on('socket-close', function(code, message) {
+    data.onDisconnection(code,message);
+  });
+
+  ddp.on('socket-error', function(error) {
+    data.onConnetionError(error);
+  });
+  if(func !== undefined) func();
+};
+
+
+module.exports = Server;
